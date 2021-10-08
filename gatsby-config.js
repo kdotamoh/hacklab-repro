ae0ec86c5db65c596a16329ceea977f44957340c
kdotamoh <kwadwo.amoh@gmail.com>
2021-10-08 09:31:20 +0000

    chore: update config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,26 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Hacklab Foundation Website",
+    siteUrl: `${process.env.SERVER_ADDRESS}`,
+    title: 'Hacklab Foundation Website',
   },
   plugins: [
     {
-      resolve: "gatsby-source-wordpress",
+      resolve: 'gatsby-source-wordpress',
       options: {
         url: `${process.env.SERVER_ADDRESS}/graphql`,
       },
     },
-    "gatsby-plugin-theme-ui",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-theme-ui',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
   ],
-};
+}
