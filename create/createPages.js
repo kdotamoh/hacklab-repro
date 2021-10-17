d6460e3bf6ba6eb1fee5c39eb424fc5108c38e1a
kdotamoh <kwadwo.amoh@gmail.com>
2021-10-17 00:29:28 +0000

    create pages

---
+++ create/createPages.js
@@ -0,0 +1,105 @@
+const pageTemplate = require.resolve('../src/templates/page')
+
+const GET_PAGES = `
+  query GET_PAGES {
+    allWpPage {
+      nodes {
+        title
+        uri
+        isFrontPage
+      }
+    }
+  }
+`
+
+const allPages = []
+
+/**
+ * This is the export which Gatbsy will use to process.
+ *
+ * @param { actions, graphql }
+ * @returns {Promise<void>}
+ */
+module.exports = async ({ actions, graphql, reporter }) => {
+  /**
+   * This is the method from Gatsby that we're going
+   * to use to create pages in our static site.
+   */
+  const { createPage } = actions
+  /**
+   * Fetch pages method. This accepts variables to alter
+   * the query. The variable `first` controls how many items to
+   * request per fetch and the `after` controls where to start in
+   * the dataset.
+   *
+   * @param variables
+   * @returns {Promise<*>}
+   */
+  const fetchPages = async () =>
+    /**
+     * Fetch pages using the GET_PAGES query and the variables passed in.
+     */
+    await graphql(GET_PAGES).then(({ data }) => {
+      /**
+       * Extract the data from the GraphQL query results
+       */
+      const {
+        allWpPage: { nodes },
+      } = data
+
+      /**
+       * Map over the pages for later creation
+       */
+      nodes &&
+        nodes.map((pages) => {
+          allPages.push(pages)
+        })
+      /**
+       * If there's another page, fetch more
+       * so we can have all the data we need.
+       */
+      // if (hasNextPage) {
+      //   pageNumber++
+      //   reporter.info(`fetch page ${pageNumber} of pages...`)
+      //   return fetchPages({ first: itemsPerPage, after: endCursor })
+      // }
+
+      /**
+       * Once we're done, return all the pages
+       * so we can create the necessary pages with
+       * all the data on hand.
+       */
+      return allPages
+    })
+
+  /**
+   * Kick off our `fetchPages` method which will get us all
+   * the pages we need to create individual pages.
+   */
+  await fetchPages().then((wpPages) => {
+    wpPages &&
+      wpPages.map((page) => {
+        let pagePath = `${page.uri}`
+
+        /**
+         * If the page is the front page, the page path should not be the uri,
+         * but the root path '/'.
+         */
+        if (page.isFrontPage) {
+          pagePath = '/'
+        }
+
+        createPage({
+          path: pagePath,
+          component: pageTemplate,
+          context: {
+            page: page,
+          },
+        })
+
+        reporter.info(`page created: ${page.uri}`)
+      })
+
+    reporter.info(`# -----> PAGES TOTAL: ${wpPages.length}`)
+  })
+}
---
+++ src/templates/page/index.js
@@ -0,0 +1,14 @@
+/** @jsxImportSource theme-ui */
+import * as React from 'react'
+
+import Layout from '../../components/Layout'
+
+const Page = ({ pageContext }) => {
+
+  return (
+    <Layout>
+    </Layout>
+  )
+}
+
+export default Page
