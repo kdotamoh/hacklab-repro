e2e1186663806b2bc5682826427463702c4ae016
kdotamoh <kwadwo.amoh@gmail.com>
2021-12-07 10:38:55 +0000

    add store context

--- src/context/Store.js
+++ src/context/Store.js
@@ -1,5 +1,6 @@
 /** @jsxImportSource theme-ui */
 import * as React from 'react'
+import { isEmpty } from 'lodash'
 
 /**
  * @param {Object} props
@@ -10,21 +11,7 @@ import * as React from 'react'
  * @param {Object} props.pageContext.page.pageBuilder
  * @param {Object[]} defaultValues.cart
  */
-const defaultValues = {
-  cart: [],
-  isOpen: false,
-  loading: false,
-  didJustAddToCart: false,
-  onOpen: () => {},
-  onClose: () => {},
-  addVariantToCart: () => {},
-  // addToCart: () => {},
-  removeLineItem: () => {},
-  updateLineItem: () => {},
-  checkout: {
-    lineItems: [],
-  },
-}
+const defaultValues = {}
 
 export const StoreContext = React.createContext(defaultValues)
 
@@ -34,10 +21,6 @@ export const StoreProvider = ({ children }) => {
    */
   const [checkout, setCheckout] = React.useState(defaultValues.checkout)
   const [cart, setCart] = React.useState(defaultValues.cart)
-  const [loading, setLoading] = React.useState(false)
-  const [didJustAddToCart, setDidJustAddToCart] = React.useState(false)
-
-  const addVariantToCart = () => {}
 
   /**
    *
@@ -45,32 +28,97 @@ export const StoreProvider = ({ children }) => {
    * @param {Object} params.product
    * @param {Number} params.quantity
    */
-  const addToCart = ({ product, quantity }) => {
-    const checkId = (obj) => obj.id === product.id
+  const addToCart = ({ product, product_id, quantity, variation_id }) => {
+    const checkId = (obj) => obj.product_id === product.id
     if (cart.some(checkId)) {
       alert('The item you are trying to add is already in your cart')
       return false
     } else {
-      // const newCart = [product]
-      setCart([...cart, { product, quantity }])
+      setCart([...cart, { product, product_id, quantity, variation_id }])
+      localStorage.setItem(
+        'hacklab_cart',
+        JSON.stringify([
+          ...cart,
+          { product, product_id, quantity, variation_id },
+        ])
+      )
     }
   }
 
-  const removeLineItem = () => {}
-  const updateLineItem = () => {}
+  const getLocalCart = () => {
+    // grab localCart from localStorage
+    const cachedCart = localStorage.getItem('hacklab_cart')
+    // if so, use cached
+    if (cachedCart && cachedCart.length !== 0) {
+      setCart(JSON.parse(cachedCart))
+      console.log('Cart: Using cached!')
+    } else {
+      setCart([])
+    }
+  }
+
+  const getCheckout = () => {
+    const cachedCheckout = sessionStorage.getItem('hacklab_checkout')
+
+    if (cachedCheckout && !isEmpty(cachedCheckout)) {
+      setCheckout(JSON.parse(cachedCheckout))
+      console.log('Checkout: Using cached!')
+    } else {
+      setCheckout({})
+    }
+  }
+
+  const proceedToCheckout = (order) => {
+    setCheckout(order)
+    sessionStorage.setItem('hacklab_checkout', JSON.stringify(order))
+  }
+
+  const removeLineItem = (product_id) => {
+    const newCart = cart.filter((product, productIndex) => {
+      return product.product_id !== product_id
+    })
+    setCart(newCart)
+    localStorage.setItem('hacklab_cart', JSON.stringify(newCart))
+  }
+
+  const updateQuantity = ({ product_id, quantity }) => {
+    let newCart = cart
+    let objIndex = newCart.findIndex((obj) => obj.product_id === product_id)
+    newCart[objIndex].quantity += 1
+
+    setCart([...newCart])
+    localStorage.setItem('hacklab_cart', JSON.stringify(newCart))
+  }
+
+  const reduceQuantity = ({ product_id, quantity }) => {
+    let newCart = cart
+    let objIndex = newCart.findIndex((obj) => obj.product_id === product_id)
+
+    if (newCart[objIndex].quantity > 1) {
+      newCart[objIndex].quantity -= 1
+    }
+
+    setCart([...newCart])
+    localStorage.setItem('hacklab_cart', JSON.stringify(newCart))
+  }
+
+  React.useEffect(() => {
+    getLocalCart()
+    getCheckout()
+  }, [])
 
   return (
     <StoreContext.Provider
       value={{
         ...defaultValues,
-        addVariantToCart,
         addToCart,
+        getLocalCart,
         removeLineItem,
-        updateLineItem,
+        updateQuantity,
+        reduceQuantity,
         checkout,
+        proceedToCheckout,
         cart,
-        loading,
-        didJustAddToCart,
       }}
     >
       {children}
