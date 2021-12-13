793eb1cb02927146ba81e6672efb69f5a175fa9c
kdotamoh <kwadwo.amoh@gmail.com>
2021-12-13 05:16:45 +0000

    check if cart is empty on checkout page

--- src/pages/store/checkout/index.js
+++ src/pages/store/checkout/index.js
@@ -6,8 +6,11 @@ import Layout from '../../../components/Layout'
 import Menu from '../../../components/store/layout/menu'
 import Form from '../../../components/store/checkout/form'
 import OrderDetails from '../../../components/store/checkout/order-details'
+import { StoreContext } from '../../../context/Store'
 
 const Checkout = () => {
+  const { cart } = React.useContext(StoreContext)
+
   return (
     <Layout>
       <Menu />
@@ -16,16 +19,30 @@ const Checkout = () => {
           width: ['92%', '92%', '82%'],
         }}
       >
-        <div
-          sx={{
-            display: 'grid',
-            gridTemplateColumns: '1fr 1fr',
-            gap: '3.25rem',
-          }}
-        >
-          <Form />
-          <OrderDetails />
-        </div>
+        {cart.length > 0 ? (
+          <div
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: '1fr 1fr',
+              gap: '3.25rem',
+            }}
+          >
+            <Form />
+            <OrderDetails />
+          </div>
+        ) : (
+          <>
+            <h4
+              sx={{
+                mt: '2.5rem',
+                mb: '1rem',
+              }}
+            >
+              Your cart is empty
+            </h4>
+            <p>Please add some items to your cart.</p>
+          </>
+        )}
       </Container>
     </Layout>
   )
