d3e294a569b4c2c87e47c2a80309c6c5c681c924
kdotamoh <kwadwo.amoh@gmail.com>
2022-01-04 11:21:34 +0000

    update header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -40,185 +40,189 @@ const Header = ({
           themeColor={themeColor}
         />
       )}
-      <div
-        {...props}
-        sx={{
-          ...(fullHeight && { height: '92vh' }),
-          minHeight: 'max-content',
-          color: `neutral.${textColor}`,
-          ...(backgroundImage && {
-            backgroundImage: `url(${backgroundImage.sourceUrl})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }),
-          backgroundColor: backgroundColor ? backgroundColor : 'neutral.white',
-          display: 'grid',
-          gridTemplateAreas: `'navigation'
-                            'body'`,
-          gridTemplateRows: 'max-content 1fr',
-          position: 'relative',
-        }}
-      >
-        {inHeader && (
-          <Navigation
-            logoUrl={logo?.sourceUrl}
-            sx={{
-              gridArea: 'navigation',
-            }}
-            color={navigationColor}
-            themeColor={themeColor}
-          />
-        )}
-
-        <Container
+      {!headerText && !image ? null : (
+        <div
+          {...props}
           sx={{
-            ...(isFrontPage && { transform: 'translateY(-5rem)' }),
-            alignSelf: 'center',
-            gridArea: 'body',
-            alignItems: 'center',
-            minHeight: '20rem',
-            width: ['92%', '92%', '82%'],
-            pb: ['2rem', '2rem', '5rem'],
-            ...(image && {
-              display: 'grid',
-              gap: '4rem 7rem',
-              ...(image &&
-                position === 'topLogo' && {
-                  gap: '0rem',
-                }),
-              ...(image &&
-                position === 'right' && {
-                  gridTemplateColumns: ['100%', '100%', '50% 1fr'],
-                }),
-              gridTemplateAreas:
-                position === 'bottom'
-                  ? `"content"
-                    "image"`
-                  : position === 'top'
-                  ? `"image"
-                    "content"`
-                  : position === 'topLogo'
-                  ? `"image"
-                    "content"`
-                  : position === 'right'
-                  ? [
-                      `"content"
-                       "image"`,
-                      `"content"
-                       "image"`,
-                      `"content image"`,
-                    ]
-                  : ``,
+            ...(fullHeight && { height: '92vh' }),
+            minHeight: 'max-content',
+            color: `neutral.${textColor}`,
+            ...(backgroundImage && {
+              backgroundImage: `url(${backgroundImage.sourceUrl})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }),
-            ...(textAlignment === 'left' && {
-              display: 'grid',
-              gridTemplateColumns: ['100%', '100%', '50% 1fr'],
-              gridTemplateAreas: [
-                `"content"
-                "image"`,
-                `"content"
-                "image"`,
-                `"content image"`,
-              ],
-            }),
-            // the styling's pretty messy here, clean this up
+            backgroundColor: backgroundColor
+              ? backgroundColor
+              : 'neutral.white',
+            display: 'grid',
+            gridTemplateAreas: `'navigation'
+                            'body'`,
+            gridTemplateRows: 'max-content 1fr',
+            position: 'relative',
           }}
         >
-          <div
+          {inHeader && (
+            <Navigation
+              logoUrl={logo?.sourceUrl}
+              sx={{
+                gridArea: 'navigation',
+              }}
+              color={navigationColor}
+              themeColor={themeColor}
+            />
+          )}
+
+          <Container
             sx={{
-              textAlign: textAlignment,
-              gridArea: 'content',
-              mt: '5rem',
+              ...(isFrontPage && { transform: 'translateY(-5rem)' }),
+              alignSelf: 'center',
+              gridArea: 'body',
+              alignItems: 'center',
+              minHeight: '20rem',
+              width: ['92%', '92%', '82%'],
+              pb: ['2rem', '2rem', '5rem'],
+              ...(image && {
+                display: 'grid',
+                gap: '4rem 7rem',
+                ...(image &&
+                  position === 'topLogo' && {
+                    gap: '0rem',
+                  }),
+                ...(image &&
+                  position === 'right' && {
+                    gridTemplateColumns: ['100%', '100%', '50% 1fr'],
+                  }),
+                gridTemplateAreas:
+                  position === 'bottom'
+                    ? `"content"
+                    "image"`
+                    : position === 'top'
+                    ? `"image"
+                    "content"`
+                    : position === 'topLogo'
+                    ? `"image"
+                    "content"`
+                    : position === 'right'
+                    ? [
+                        `"content"
+                       "image"`,
+                        `"content"
+                       "image"`,
+                        `"content image"`,
+                      ]
+                    : ``,
+              }),
+              ...(textAlignment === 'left' && {
+                display: 'grid',
+                gridTemplateColumns: ['100%', '100%', '50% 1fr'],
+                gridTemplateAreas: [
+                  `"content"
+                "image"`,
+                  `"content"
+                "image"`,
+                  `"content image"`,
+                ],
+              }),
+              // the styling's pretty messy here, clean this up
             }}
           >
             <div
               sx={{
-                h1: {
-                  fontSize: isFrontPage ? 'display2' : 'h2',
-                },
-                p: {
-                  pt: '1.5rem',
-                  lineHeight: 'h3',
-                },
+                textAlign: textAlignment,
+                gridArea: 'content',
+                mt: '5rem',
               }}
-              dangerouslySetInnerHTML={{ __html: headerText }}
-            />
-            {buttons && (
+            >
               <div
                 sx={{
-                  display: 'flex',
-                  flexWrap: 'wrap',
-                  gap: '.75rem',
-                  justifyContent: textAlignment,
-                  mt: '2rem',
+                  h1: {
+                    fontSize: isFrontPage ? 'display2' : 'h2',
+                  },
+                  p: {
+                    pt: '1.5rem',
+                    lineHeight: 'h3',
+                  },
                 }}
-              >
-                {playStoreLink && (
-                  <DownloadBadge
-                    // @ts-ignore
-                    onClick={() => window.open(playStoreLink)}
-                    appStore="google"
-                    width="10rem"
-                  />
-                )}
-                {appStoreLink && (
-                  <DownloadBadge
-                    // @ts-ignore
-                    onClick={() => window.open(playStoreLink)}
-                    appStore="apple"
-                    width="10rem"
-                  />
-                )}
-                {buttons.map((button, index) => (
-                  <Link
-                    sx={{
-                      textDecoration: 'none',
-                    }}
-                    key={index}
-                    to={button.linkUrl}
-                  >
-                    <Button
+                dangerouslySetInnerHTML={{ __html: headerText }}
+              />
+              {buttons && (
+                <div
+                  sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    gap: '.75rem',
+                    justifyContent: textAlignment,
+                    mt: '2rem',
+                  }}
+                >
+                  {playStoreLink && (
+                    <DownloadBadge
+                      // @ts-ignore
+                      onClick={() => window.open(playStoreLink)}
+                      appStore="google"
+                      width="10rem"
+                    />
+                  )}
+                  {appStoreLink && (
+                    <DownloadBadge
+                      // @ts-ignore
+                      onClick={() => window.open(playStoreLink)}
+                      appStore="apple"
+                      width="10rem"
+                    />
+                  )}
+                  {buttons.map((button, index) => (
+                    <Link
                       sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        height: '100%',
+                        textDecoration: 'none',
                       }}
-                      variant={button.buttonVariant}
+                      key={index}
+                      to={button.linkUrl}
                     >
-                      {button.buttonIcon && (
-                        <img
-                          sx={{
-                            height: '1.25rem',
-                            width: '1.25rem',
-                            mr: '.5rem',
-                          }}
-                          src={button.buttonIcon.sourceUrl}
-                          alt=""
-                        />
-                      )}
-                      <span>{button.buttonText}</span>
-                    </Button>
-                  </Link>
-                ))}
-              </div>
+                      <Button
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          height: '100%',
+                        }}
+                        variant={button.buttonVariant}
+                      >
+                        {button.buttonIcon && (
+                          <img
+                            sx={{
+                              height: '1.25rem',
+                              width: '1.25rem',
+                              mr: '.5rem',
+                            }}
+                            src={button.buttonIcon.sourceUrl}
+                            alt=""
+                          />
+                        )}
+                        <span>{button.buttonText}</span>
+                      </Button>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+            {image && (
+              <img
+                sx={{
+                  width: '100%',
+                  ...(position === 'topLogo' && {
+                    maxWidth: '10rem',
+                    justifySelf: 'center',
+                  }),
+                  gridArea: 'image',
+                }}
+                src={image?.sourceUrl}
+                alt=""
+              />
             )}
-          </div>
-          {image && (
-            <img
-              sx={{
-                width: '100%',
-                ...(position === 'topLogo' && {
-                  maxWidth: '10rem',
-                  justifySelf: 'center',
-                }),
-                gridArea: 'image',
-              }}
-              src={image?.sourceUrl}
-              alt=""
-            />
-          )}
-        </Container>
-      </div>
+          </Container>
+        </div>
+      )}
     </>
   )
 }
