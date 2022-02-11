682b4defa2ca7f936bcb89c483507dcf76f73258
kdotamoh <kwadwo.amoh@gmail.com>
2022-02-11 17:55:34 +0000

    update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,25 +14,6 @@ module.exports = {
         url: `${process.env.GATSBY_SERVER_ADDRESS}/graphql`,
         schema: {
           timeout: 30000,
-          perPage: 500,
-          requestConcurrency: 5,
-        },
-      },
-      develop: { hardCacheMediaFiles: true },
-      type: {
-        MediaItem: {
-          ...(process.env.NODE_ENV === 'development' && {
-            localFile: {
-              maxFileSizeBytes: 0,
-              excludeByMimeTypes: [
-                `application/pdf`,
-                `image/jpeg`,
-                `image/png`,
-                `image/svg+xml`,
-              ],
-            },
-          }),
-          requestConcurrency: 200,
         },
       },
     },
